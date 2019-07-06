c4a54fa4ce73bc50d7c12e3d219eb10bed7621a8
LaysCR <layscribeiro@gmail.com>
2019-07-06 20:00:52 -0300

    Berry PWM in progress

--- Berry.py
+++ Berry.py
@@ -6,80 +6,81 @@ from Berry_acceleration import Accelerometer
 from Berry_compass import Compass
 from Berry_movement import Movement
 
+
 # Move foward and detect obstacles
 def moveFoward():
     x = []
     y = []
+    z = []
     angle = []
     dist = []
     distance = ultrasonicSensor.getDistance()
-    
-    while(distance > 20):
-##        print("Distance = %1.f cm" % distance)
+
+    while (distance > 30):
+        #        print("Distance = %1.f cm" % distance)
         dist.append(int(distance))
-        motors.move(8, 0.2) # Move foward
+        motors.move(8, 0.2)  # Move foward
         axes = accelerationSensor.getAcceleration()
         x.append(axes['y'])
         y.append(axes['x'])
-        time.sleep(0.5)
+        # time.sleep(0.2)
         distance = ultrasonicSensor.getDistance()
-    for i in range(len(x)):
-        x[i] = abs(x[i])
-        y[i] = abs(y[i])
     # Velocity
     Vx = []
     Vy = []
     t = 0.2
     for i in range(0, len(x)):
-        if(i == 0):
-            Vx.append(x[i]*t/2)
-            Vy.append(x[i]*t/2)
+        if (i == 0):
+            Vx.append(x[i] * t / 2)
+            Vy.append(x[i] * t / 2)
         else:
-            Vx.append((x[i] + x[i-1])*t/2)
-            Vy.append((y[i] + y[i-1])*t/2)
+            Vx.append((x[i] + x[i - 1]) * t / 2)
+            Vy.append((y[i] + y[i - 1]) * t / 2)
     # Distance
     Sx = []
     Sy = []
     for i in range(0, len(Vx)):
-        if(i == 0):
-            Sx.append(Vx[i]*t/2)
-            Sy.append(Vy[i]*t/2)
+        if (i == 0):
+            Sx.append(Vx[i] * t / 2)
+            Sy.append(Vy[i] * t / 2)
         else:
-            Sx.append((Vx[i] + Vx[i-1])*t/2)
-            Sy.append((Vy[i] + Vy[i-1])*t/2)
+            Sx.append((Vx[i] + Vx[i - 1]) * t / 2)
+            Sy.append((Vy[i] + Vy[i - 1]) * t / 2)
     Dx = 0
     Dy = 0
     DeltaS = 0
     for i in range(len(Sx)):
-        Dx += Sx[i]
-        Dy += Sy[i]
-        S = math.sqrt(Sx[i]*Sx[i] + Sy[i]*Sy[i])
-        DeltaS += S*100 # Convert from m to cm
+        Dx += Sx[i] * 100
+        Dy += Sy[i] * 100
+        S = math.sqrt(Sx[i] * Sx[i] + Sy[i] * Sy[i])
+        DeltaS += S * 100  # Convert from m to cm
 
     # Each instant is 0.2s (from the sleep(0.2) in move method)
-##    print(x)
-##    print(y)
-##    print(Vx)
-##    print(Vy)
-##    print(Sx)
-##    print(Sy)
+    print(x)
+    print(y)
+    #    print(Vx)
+    #    print(Vy)
+    #    print(Sx)
+    #    print(Sy)
     print(Dx)
     print(Dy)
     print(DeltaS)
     return DeltaS
 
+
 # Check side
 def selectSide(side, seconds):
     angle = gyroscopeSensor.getAngle()
-    print("Angle = %d" % angle)
+    #    print("Angle = %d" % angle)
     motors.move(side, seconds)
     distance = ultrasonicSensor.getDistance()
-    print(distance)
+    #    print(distance)
     angle = gyroscopeSensor.getAngle()
-    print("Angle = %d" % angle)
+    #    print("Angle = %d" % angle)
     time.sleep(0.5)
     return distance, angle
-    
+
+
 try:
     # Instantiate objects
     motors = Movement()
@@ -87,21 +88,36 @@ try:
     gyroscopeSensor = Compass()
     accelerationSensor = Accelerometer()
 
-    initialAngle = gyroscopeSensor.getAngle()
-    # Choose which side to turn
-    seconds = 0.2
-    (distanceLeft, angleLeft) = selectSide(4, seconds) #(side, seconds)
-    (distanceRight, angleRight) = selectSide(6, 2*seconds) #(side, 2*seconds)
-    if(distanceLeft > distanceRight):
-        # Choose left
-        motors.move(4, 2*seconds)
-        time.sleep(1)
-        D = moveFoward()
-    else:
-        # Choose right
-        time.sleep(1)
-        D = moveFoward()
-    
+    #    print(accelerationSensor.getAcceleration())
+
+    D = moveFoward()
+
+#    iterations = 5
+#    seconds = 0.2
+#    while(iterations > 0):
+#        iterations -= 1
+#        initialAngle = gyroscopeSensor.getAngle()
+#        # Choose which side to turn
+#        (distanceLeft, angleLeft) = selectSide(4, seconds) #(side, seconds)
+#        (distanceRight, angleRight) = selectSide(6, 2*seconds) #(side, 2*seconds)
+#        if(distanceLeft > distanceRight and distanceLeft > 30):
+#            # Choose left
+#            motors.move(4, 2*seconds)
+#            time.sleep(1)
+#            newAngle = gyroscopeSensor.getAngle()
+#            D = moveFoward()
+#            seconds = 0.2
+#        elif(distanceRight > 30):
+#            # Choose right
+#            time.sleep(1)
+#            newAngle = gyroscopeSensor.getAngle()
+#            D = moveFoward()
+#            seconds = 0.2
+#        else:
+#            iterations += 1
+#            seconds += 0.2
+#        print(D)
+
 # Reset by pressing CTRL + C
 except KeyboardInterrupt:
     print("Measurement stopped by User")
--- Berry_movement.py
+++ Berry_movement.py
@@ -1,9 +1,13 @@
 import RPi.GPIO as GPIO
+import pigpio
 import threading
 import time
+from Berry_distance import Distance
 
-left = [19, 20, 21]
-right = [22, 23, 24]
+left = [19, 20, 21]     # RIGHT
+right = [22, 23, 24]    # LEFT
+freq = 10000
+pi = pigpio.pi()
 
 class Movement(threading.Thread):
     # Constructor
@@ -16,39 +20,61 @@ class Movement(threading.Thread):
         for port in ports:
             GPIO.setup(port, GPIO.OUT)
 
+# pi.set_PWM_dutycycle(4,   0) # PWM off
+# pi.set_PWM_dutycycle(4,  64) # PWM 1/4 on
+# pi.set_PWM_dutycycle(4, 128) # PWM 1/2 on
+# pi.set_PWM_dutycycle(4, 192) # PWM 3/4 on
+# pi.set_PWM_dutycycle(4, 255) # PWM full on
+
     # Foward
     def foward(self, side):
         self.enablePins(side)
+        if side == left: #RIGHT
+            # pi.set_PWM_dutycycle(19, 255)
+            pwmLeft = GPIO.PWM(19, freq)
+            pwmLeft.ChangeDutyCycle(100)
+        elif side == right:  #LEFT
+            # pi.set_PWM_dutycycle(22, 128)
+            pwmRight = GPIO.PWM(22, freq)
+            pwmRight.ChangeDutyCycle(10)
         GPIO.output(side[0], True)
         GPIO.output(side[1], True)
         GPIO.output(side[2], False)
-        
+
     # Reverse
     def reverse(self, side):
         self.enablePins(side)
+        # if side == left:
+        #     pwmLeft = GPIO.PWM(19, freq)
+        #     pwmLeft.ChangeDutyCycle(100)
+        # elif side == right:
+        #     pwmRight = GPIO.PWM(22, freq)
+        #     pwmRight.ChangeDutyCycle(10)
         GPIO.output(side[0], True)
         GPIO.output(side[1], False)
         GPIO.output(side[2], True)
 
     # Movement
-    def move(self, direction):
-        if(direction == 8):
-            self.foward(left)
-            self.foward(right)
-        elif(direction == 2):
-            self.reverse(left)
-            self.reverse(right)
-        elif(direction == 6):
-            self.foward(right)
-            self.reverse(left)
-        elif(direction == 4):
-            self.foward(left)
-            self.reverse(right)
-        else:
-            print("Direcao invalida!!")
-        time.sleep(0.2)
+    def move(self, direction, seconds):
+        stop = time.time() + seconds
+        while(time.time() < stop):
+            if(direction == 8):
+                self.foward(left)
+                self.foward(right)
+            elif(direction == 2):
+                self.reverse(left)
+                self.reverse(right)
+            elif(direction == 6):
+                self.foward(right)
+                self.reverse(left)
+            elif(direction == 4):
+                self.foward(left)
+                self.reverse(right)
+            else:
+                print("Direcao invalida!!")
+#        time.sleep(0.2)
         GPIO.cleanup()
-    
+
     # Thread
     def run(self):
         print("foward")
@@ -63,4 +89,3 @@ class Movement(threading.Thread):
         print("right")
         self.move(6)
         time.sleep(2)
-        
