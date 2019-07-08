6cf9d09b706678550819907399127bd057cd5e37
LaysCR <layscribeiro@gmail.com>
2019-07-07 23:08:21 -0300

    Walking Straight

--- Motors.py
+++ Motors.py
@@ -3,74 +3,51 @@ import time
 from Berry_compass import Compass
 
 compass = Compass()
-seconds = 2
+seconds = 6
+secondsTurn = 0.2
 
 left = Motor(enable=22, forward=23, backward=24)
 right = Motor(enable=19, forward=20, backward=21)
 
 
 def moveFoward():
+    error = 90
+    stopTurning = time.time() + secondsTurn
+    if compass.getAngle() - error < 0:
+        while time.time() < stopTurning:
+            left.reverse()
+            right.forward()
+        left.stop()
+        right.stop()
+    elif compass.getAngle() + error > 360:
+        while time.time() < stopTurning:
+            left.forward()
+            right.reverse()
+        left.stop()
+        right.stop()
 
-    stop = time.time() + seconds
-    angle = compass.getAngle()
-    print("INICIAL = ", angle)
     speedLeft = 0.6
     speedRight = 0.4
-    error = 10
-    limit = 0
-    correction = False
-
+    angle = compass.getAngle()
+    stop = time.time() + seconds
     while time.time() < stop:
-        print(speedLeft)
-        print(speedRight)
         left.forward(speedLeft)
         right.forward(speedRight)
         newAngle = compass.getAngle()
-        print("NOVO INICIAL = ", newAngle)
         if newAngle != angle:
-            # Look for special condition
-            if angle + error > 360:
-                correction = True
-                limit = abs(360 - (angle + error))
-                print(limit)
-            elif angle - error < 0:
-                correction = True
-                limit = 360 - abs(angle - error)
-                print(limit)
-            # Regular condition
-            if correction is False:
-                # Correct to Left
-                if newAngle > angle:
-                    if speedLeft != 0.9:
-                        speedLeft += 0.1
-                        speedRight -= 0.1
-                # Correct to Right
-                else:
-                    if speedRight != 0.9:
-                        speedLeft -= 0.1
-                        speedRight += 0.1
-            # Special condition
+            # Correct to Left
+            if newAngle > angle:
+                if speedLeft != 0.9:
+                    speedLeft += 0.1
+                    speedRight -= 0.1
+            # Correct to Right
             else:
-                # Correct to Left
-                if limit < newAngle < angle or newAngle > limit > angle or limit > angle > newAngle:
-                    print("ESQUERDA", limit)
-                    if speedRight != 0.9:
-                        speedLeft -= 0.1
-                        speedRight += 0.1
-                # Correct to Right
-                elif limit < angle < newAngle or angle < newAngle < limit or newAngle < limit < angle:
-                    print("DIREITA", limit)
-                    if speedLeft != 0.9:
-                        speedLeft += 0.1
-                        speedRight -= 0.1
+                if speedRight != 0.9:
+                    speedLeft -= 0.1
+                    speedRight += 0.1
 
-        left.stop()
-        right.stop()
-        speedLeft = round(speedLeft, 2)
-        speedRight= round(speedRight, 2)
-        angle = compass.getAngle()
-        print("ANGULO FINAL = ", angle)
-        # time.sleep(2)
+            speedLeft = round(speedLeft, 2)
+            speedRight = round(speedRight, 2)
 
 
 try:
