66c1da735e3b1be429e1d50adb69479334871492
LaysCR <layscribeiro@gmail.com>
2019-07-07 23:44:06 -0300

    Controling DC motors with gpiozero library

--- Berry_movement.py
+++ Berry_movement.py
@@ -1,91 +1,81 @@
-import RPi.GPIO as GPIO
-import pigpio
+from gpiozero import Motor
 import threading
 import time
-from Berry_distance import Distance
+from Berry_compass import Compass
 
-left = [19, 20, 21]     # RIGHT
-right = [22, 23, 24]    # LEFT
-freq = 10000
-pi = pigpio.pi()
+compass = Compass()
+seconds = 4
+secondsTurn = 0.5
+
+left = Motor(enable=22, forward=23, backward=24)
+right = Motor(enable=19, forward=20, backward=21)
 
 class Movement(threading.Thread):
     # Constructor
     def __init__(self):
         threading.Thread.__init__(self)
 
-    # Enable pins
-    def enablePins(self, ports):
-        GPIO.setmode(GPIO.BCM)
-        for port in ports:
-            GPIO.setup(port, GPIO.OUT)
+    # Go Straight
+    def moveFoward(self, seconds):
+        error = 90
+        if compass.getAngle() - error < 0:
+            # Turn Left
+            turnLeft(secondsTurn)
+        elif compass.getAngle() + error > 360:
+            # Turn Right
+            turnRight(secondsTurn)
 
-# pi.set_PWM_dutycycle(4,   0) # PWM off
-# pi.set_PWM_dutycycle(4,  64) # PWM 1/4 on
-# pi.set_PWM_dutycycle(4, 128) # PWM 1/2 on
-# pi.set_PWM_dutycycle(4, 192) # PWM 3/4 on
-# pi.set_PWM_dutycycle(4, 255) # PWM full on
-
-    # Foward
-    def foward(self, side):
-        self.enablePins(side)
-        if side == left: #RIGHT
-            # pi.set_PWM_dutycycle(19, 255)
-            pwmLeft = GPIO.PWM(19, freq)
-            pwmLeft.ChangeDutyCycle(100)
-        elif side == right:  #LEFT
-            # pi.set_PWM_dutycycle(22, 128)
-            pwmRight = GPIO.PWM(22, freq)
-            pwmRight.ChangeDutyCycle(10)
-        GPIO.output(side[0], True)
-        GPIO.output(side[1], True)
-        GPIO.output(side[2], False)
-
-    # Reverse
-    def reverse(self, side):
-        self.enablePins(side)
-        # if side == left:
-        #     pwmLeft = GPIO.PWM(19, freq)
-        #     pwmLeft.ChangeDutyCycle(100)
-        # elif side == right:
-        #     pwmRight = GPIO.PWM(22, freq)
-        #     pwmRight.ChangeDutyCycle(10)
-        GPIO.output(side[0], True)
-        GPIO.output(side[1], False)
-        GPIO.output(side[2], True)
-
-    # Movement
-    def move(self, direction, seconds):
+        speedLeft = 0.6
+        speedRight = 0.4
+        angle = compass.getAngle()
         stop = time.time() + seconds
-        while(time.time() < stop):
-            if(direction == 8):
-                self.foward(left)
-                self.foward(right)
-            elif(direction == 2):
-                self.reverse(left)
-                self.reverse(right)
-            elif(direction == 6):
-                self.foward(right)
-                self.reverse(left)
-            elif(direction == 4):
-                self.foward(left)
-                self.reverse(right)
-            else:
-                print("Direcao invalida!!")
-#        time.sleep(0.2)
-        GPIO.cleanup()
+        while time.time() < stop:
+            left.forward(speedLeft)
+            right.forward(speedRight)
+            newAngle = compass.getAngle()
+            if newAngle != angle:
+                # Correct to Left
+                if newAngle > angle:
+                    if speedLeft != 0.9:
+                        speedLeft += 0.1
+                        speedRight -= 0.1
+                # Correct to Right
+                else:
+                    if speedRight != 0.9:
+                        speedLeft -= 0.1
+                        speedRight += 0.1
+
+                speedLeft = round(speedLeft, 2)
+                speedRight = round(speedRight, 2)
+        left.stop()
+        right.stop()
+
+    # Turn Left
+    def turnLeft(self, secondsTurn):
+        stopTurning = time.time() + secondsTurn
+        while time.time() < stopTurning:
+            left.backward()
+            right.forward()
+        left.stop()
+        right.stop()
+
+    # Turn Right
+    def turnRight(self, secondsTurn):
+        stopTurning = time.time() + secondsTurn
+        while time.time() < stopTurning:
+            left.forward()
+            right.backward()
+        left.stop()
+        right.stop()
 
     # Thread
     def run(self):
-        print("foward")
-        self.move(8)
-        time.sleep(2)
-        print("reverse")
-        self.move(2)
-        time.sleep(2)
-        print("left")
-        self.move(4)
-        time.sleep(2)
-        print("right")
-        self.move(6)
-        time.sleep(2)
+        try:
+            moveFoward(seconds)
+            turnLeft(secondsTurn)
+            turnRight(secondsTurn)
+            turnRight(secondsTurn)
+            turnLeft(secondsTurn)
+        # Reset by pressing CTRL + C
+        except KeyboardInterrupt:
+            print("Measurement stopped by User")
