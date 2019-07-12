279a0293fb4f8161e32d9782d743f6ff6e617a8b
LaysCR <layscribeiro@gmail.com>
2019-07-12 19:51:09 -0300

    DC Motors

--- Berry_gpiozero.py
+++ Berry_gpiozero.py
@@ -1,17 +1,13 @@
-from Berry_compass import Compass
-from gpiozero import DistanceSensor
-from gpiozero import Motor
-import threading
 import time
+import threading
+from gpiozero import Motor
+from Berry_compass import Compass
 
 compass = Compass()
-seconds = 2
-secondsTurn = 0.5
 
 left = Motor(enable=22, forward=23, backward=24)
 right = Motor(enable=19, forward=20, backward=21)
-# DistanceSensor(echo, trigger)
-ultrasonicSensor = DistanceSensor(echo=6, trigger=5)
+
 
 class Movement(threading.Thread):
     # Constructor
@@ -19,38 +15,24 @@ class Movement(threading.Thread):
         threading.Thread.__init__(self)
 
     # Go Forward
-    def moveForward(self, angle):
-        error = 45
-        if angle - error < 0:
-            # Turn Left
-            self.turnLeft(0.5)
-        elif angle + error > 360:
-            # Turn Right
-            self.turnRight(0.5)
-
-        speedLeft = 0.6
-        speedRight = 0.4
-        stop = time.time() + seconds
-        while ultrasonicSensor.distance*100 > 30:
-            left.forward(speedLeft)
-            right.forward(speedRight)
-            newAngle = compass.getAngle()
-            if newAngle != angle:
-                # Correct to Left
-                if newAngle > angle:
-                    if speedLeft != 0.9:
-                        speedLeft += 0.1
-                        speedRight -= 0.1
-                # Correct to Right
-                else:
-                    if speedRight != 0.9:
-                        speedLeft -= 0.1
-                        speedRight += 0.1
-
-                speedLeft = round(speedLeft, 2)
-                speedRight = round(speedRight, 2)
-        left.stop()
-        right.stop()
+    def moveForward(self, angle, speedLeft, speedRight):
+        left.forward(speedLeft)
+        right.forward(speedRight)
+        newAngle = compass.getAngle()
+        if newAngle != angle:
+            # Correct to Left
+            if newAngle > angle:
+                if speedLeft < 1:
+                    speedLeft += 0.1
+                    speedRight -= 0.1
+            # Correct to Right
+            else:
+                if speedRight < 1:
+                    speedLeft -= 0.1
+                    speedRight += 0.1
+            speedLeft = round(speedLeft, 2)
+            speedRight = round(speedRight, 2)
+        return speedLeft, speedRight
 
     # Turn Left
     def turnLeft(self, secondsTurn):
@@ -79,11 +61,12 @@ class Movement(threading.Thread):
             print("Measurement stopped by User")
 
 
-try:
-    motors = Movement()
-    angle = compass.getAngle()
-    motors.moveForward(angle)
-
-except KeyboardInterrupt:
-    print("Measurement stopped by User")
-    GPIO.cleanup()
+# try:
+#     motors = Movement()
+#     accelerationSensor = Accelerometer()
+#
+#     motors.moveForward(compass.getAngle())
+#
+# except KeyboardInterrupt:
+#     print("Measurement stopped by User")
+#     GPIO.cleanup()
